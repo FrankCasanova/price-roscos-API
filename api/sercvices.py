0001f3de0104c3881d3ef3da4f3c85569e5bb40d
frankcasanova <frankcasanova.info@gmail.com>
2022-06-13 15:17:11 +0200

    first attemp to deploy

--- api/sercvices.py
+++ api/sercvices.py
@@ -1,3 +1,5 @@
+import os
+
 import requests as _req
 from bs4 import BeautifulSoup as _bs
 from selenium import webdriver as _webdriver
@@ -26,7 +28,26 @@ MAS_URL = _req.get(
 
 
 def scraping_el_jamon():
-    webdriver = _webdriver.Firefox()
+    """
+    Function to scrape the El Jamon website
+    this function returns a dictionary with the title, price and price_kg
+    it use selenium to open the browser and scrape the website
+    instructions in:
+    https://romik-kelesh.medium.com/how-to-deploy-a-python-web-scraper-with-selenium-on-heroku-1459cb3ac76c
+    """
+
+    chrome_options = _webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+    webdriver = _webdriver.Chrome(
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options
+    )
+
     webdriver.get(
         "https://www.supermercadoseljamon.com/detalle/-/Producto/picos-finos-integrales-250g/23025302"
     )
@@ -55,6 +76,10 @@ def scraping_el_jamon():
 
 
 def scraping_dia():
+    """
+    function to scrape the DIA website
+    returns a dictionary with the title, price and price_kg
+    """
     dia_soup = _bs(DIA_URL.content, "html.parser")
     title = dia_soup.find("h1").text
     price = dia_soup.find("span", class_="big-price").text.replace("\xa0", "")
@@ -68,6 +93,10 @@ def scraping_dia():
 
 
 def scraping_carrefour():
+    """
+    function to scrape the Carrefour website
+    returns a dictionary with the title, price and price_kg
+    """
     carrefour_soup = _bs(CARREFOUR_URL.content, "html.parser")
     title = (
         carrefour_soup.find("h1", class_="product-header__name")
@@ -85,6 +114,10 @@ def scraping_carrefour():
 
 
 def scraping_mas():
+    """
+    function to scrape the Mas website
+    returns a dictionary with the title, price and price_kg
+    """
     mas_soup = _bs(MAS_URL.content, "html.parser")
     title = mas_soup.find("span", class_="base").text
     price = mas_soup.find("span", class_="price").text.replace("\xa0", "")
